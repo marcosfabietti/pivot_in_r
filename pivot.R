178f83f8cf3e7f014b297eee50dfa7563a42c3b4
mfabietti <marcos.fabietti@nuh.nhs.uk>
2024-05-22 16:48:10 +0100

    initial commit

---
+++ pivot.R
@@ -0,0 +1,23 @@
+library (readr)
+library(dplyr)
+library(tidyr)
+library(lubridate)
+
+#data extracted from https://www.kaggle.com/datasets/prasad22/daily-transactions-dataset
+
+df<- read_csv("Daily Household Transactions.csv", col_types = cols(Date = col_date(format = "%m/%d/%Y")))
+
+unique(df$Mode)
+
+df<-df %>% select(Date, Mode, Amount) %>% drop_na()
+
+pivoted<-df %>% group_by(Mode) %>%
+                     summarise(Freq = sum(Amount))
+
+pivoted_with_dates<-df %>% group_by(Mode, year = lubridate::floor_date(Date, 'year')) %>%
+                     summarise(Freq = sum(Amount))
+
+
+pivoted_with_dates_wider<-df %>% group_by(Mode, year = lubridate::floor_date(Date, 'year')) %>%
+  summarise(Freq = sum(Amount)) %>% pivot_wider(names_from = year, 
+                                                values_from = Freq, values_fill = 0)
